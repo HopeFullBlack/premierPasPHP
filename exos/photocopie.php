3504fec17191d649061229de56ffc2acae0c9a3a
Kardwane <kardwane@gmail.com>
2022-06-15 11:58:27 +0200

    Ajout exo photocopie2

--- exos/photocopie.php
+++ exos/photocopie.php
@@ -1,1 +1,58 @@
 <?php
+$prix = null;
+if (
+    isset($_POST['photocopies']) &&
+    $_POST['photocopies'] !== "" &&
+    intval($_POST['photocopies']) > 0 &&
+    intval($_POST['photocopies']) <= 100
+) {
+    $nbPhotocopie = intval($_POST['photocopies']);
+    $total = 0;
+    if ($nbPhotocopie <= 10) {
+        $total = $nbPhotocopie * 0.1;
+    } elseif ($nbPhotocopie <= 30) {
+        $total = 1 + (($nbPhotocopie - 10) * 0.09);
+    } else {
+        $total = 2.8 + (($nbPhotocopie - 30) * 0.08);
+    }
+    $prix = "Prix total : $total &euro;";
+}
+//else {
+//    echo "formulaire mal renseigné";
+//}
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Photocopie</title>
+</head>
+<body>
+<form action="" method="post">
+    <label for="photocopies">Nombre de photocopie
+        <select name="photocopies">
+            <option value="" selected>Choisir une valeur</option>
+            <?php
+            for ($i = 1; $i <= 100; $i++) {
+                echo "<option value='$i'>$i</option>";
+            }
+            ?>
+        </select>
+        <br>
+        <small>(minimum 1, maximum 100)</small>
+    </label>
+    <br>
+    <button type="submit">Calculer</button>
+</form>
+<?php
+echo $prix ?? '';
+?>
+</body>
+
+
+</html>
