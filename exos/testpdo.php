6335569fadb7b58a8fcc012b7ba9c792cbf48696
Kardwane <kardwane@gmail.com>
2022-06-21 09:27:30 +0200

    Ajout insert article

--- exos/testpdo.php
+++ exos/testpdo.php
@@ -1,18 +1,5 @@
 <?php
-//permet d'afficher les erreurs
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
-
-$dsn = 'mysql:dbname=test;host=localhost;port=3306;charset=utf8';
-
-$user = 'test';
-$pwd = 'fvi)EHDk5ya67zgO';
-
-//créer la connexion a la bdd
-$pdo = new PDO($dsn, $user, $pwd, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // permet d'activer le mode verbeux pur les erreurs
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC    // lire les enregistrement comme un tableau
-]);
+require_once 'cnxBdd.php';
 
 ////Requete simple sans parametre
 //$sql = 'select * from article';
